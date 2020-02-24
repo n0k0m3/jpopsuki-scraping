f9bcab075441c1ee9a1463428add78d579a908cd
n0k0m3 <10925298+n0k0m3@users.noreply.github.com>
2020-02-23 19:31:13 -0500

    Update download.py
    Cleanup debug

--- utils/download.py
+++ utils/download.py
@@ -4,7 +4,6 @@ import wget
 def main():
     with open('download_links.txt', 'r') as file:
         url_list = [url.rstrip('\n') for url in file.readlines()]
-        print(url_list)
         for url in url_list:
             wget.download(url)
 
